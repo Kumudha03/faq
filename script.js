2fcb4c47ef084ce16adc1973fd1fef41417f53ce
Kumudha03 <124613032+Kumudha03@users.noreply.github.com>
2024-09-20 15:15:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const containers=document.querySelectorAll('.container');
+
+containers.forEach(container=>{
+    container.addEventListener('click',()=>{
+        container.classList.toggle("active");
+    });
+});
